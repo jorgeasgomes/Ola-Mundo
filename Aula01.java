0422106a48cefb275eaca97da7f73485d5ecc025
jorgeasgomes <jgomes.anderson@gmail.com>
2021-08-12 11:40:09 -0300

    Corpo

---
+++ Aula01.java
@@ -0,0 +1,18 @@
+package aula01;
+
+public class Aula01 {
+
+  
+    public static void main(String[] args) {
+        
+        Caneta c1 = new Caneta();
+        c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.tampada = false;
+        c1.tampar();
+        c1.status();
+        c1.rabiscar();
+
+    }
+    
+}
---
+++ Caneta.java
@@ -0,0 +1,39 @@
+package aula01;
+
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+    
+    void status(){
+        System.out.println("Modelo" + this.modelo);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga:" + this.carga);
+        System.out.println("Uma Caneta" + this.cor);
+        System.out.println(" está tampanda?"+ this.tampada);
+    }
+    
+    void rabiscar(){
+        if(this.tampada == true) {
+            System.out.println("Erro. estou tampada e  não posso rabiscar");
+        } else { 
+            System.out.println("Estou rabiscando");
+        }
+    
+    }
+    
+    void tampar(){
+        this.tampada = true;
+    
+    }
+    
+    void destampar(){
+        this.tampada = false;
+    
+    }
+    
+    
+    
+}
